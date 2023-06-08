4b30311106ee0124987c92bc5ed903d29793d1f7
OwlBob <tarakankist@yandex.ru>
2023-06-08 12:36:24 +0300

    fix capitalize.test.js

--- tests/capitalize.test.js
+++ tests/capitalize.test.js
@@ -1,13 +1,15 @@
 import { capitalize } from '../src/capitalize.js';
 
+// Первое утверждение (проверка на пустую строку)
 if (capitalize('hello') !== 'Hello') {
   throw new Error('Функция работает неверно!');
 }
 
+// Второе утверждение (проверка на слово)
 if (capitalize('') !== '') {
   throw new Error('Функция работает неверно!');
 }
 
 console.log('Все тесты пройдены!');
 
-capitalize();
+capitalize('hello');
